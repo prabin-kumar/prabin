2127304107d93e86d542655c16bac39e43d1b741
prabin-kumar <hkris7989@gmail.com>
2025-07-09 14:53:03 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,8 @@
+public class multiplication {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        int product = a * b;
+        System.out.println("Product of " + a + " and " + b + " is: " + product);
+    }
+}
